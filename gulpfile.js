6dea3343cf9252246ac083f0b1f441d7ea5f5e11
DewoSadist <orynbaidaulet@gmail.com>
2017-03-08 21:16:33 +0600

    feat:add dependency

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,12 @@
 var gulp = require('gulp');
+var server = require('gulp-server-livereload');
+
+gulp.task('serv', function () {
+    gulp.src('src')
+        .pipe(server({
+            livereload: true,
+            directoryListing: false,
+            open: false,
+            port: 3000
+        }));
+});
